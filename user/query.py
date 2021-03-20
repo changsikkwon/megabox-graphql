a3310fb644646adbcee504ebc9a9714efbe73189
chang sik kwon <kcs15987@gmail.com>
2021-03-20 15:28:52 +0900

    user

---
+++ user/query.py
@@ -0,0 +1,16 @@
+from graphene_django.types import DjangoObjectType, ObjectType
+from .models import User, Like
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+
+
+class LikeType(DjangoObjectType):
+    class Meta:
+        model = Like
+
+
+class Query(ObjectType):
+    pass
