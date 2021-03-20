67830b814356d2ea9b2058e97458f594ba2454cc
chang sik kwon <kcs15987@gmail.com>
2021-03-21 02:04:13 +0900

    user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 
@@ -10,20 +11,36 @@ class TimeStampedModel(models.Model):
         ordering = ("-created_at",)
 
 
-class User(TimeStampedModel):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, account, phone_number, birth, name, password=None):
+        if not email:
+            raise ValueError("Email must be set!")
+        user = self.model(
+            email=email, account=account, phone_number=phone_number, birth=birth, name=name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def get_by_natural_key(self, account):
+        return self.get(account=account)
+
+
+class User(AbstractBaseUser, TimeStampedModel):
     name = models.CharField(max_length=50)
     birth = models.DateField()
     phone_number = models.CharField(max_length=20, unique=True)
-    account = models.CharField(max_length=50, null=True, blank=True, unique=True)
-    password = models.CharField(max_length=100)
-    email = models.EmailField(null=True, blank=True, unique=True)
-    is_user = models.BooleanField(default=True)
+    account = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
 
     class Meta:
         db_table = "user"
 
     def __str__(self):
         return self.account
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = "account"
 
 
 class Like(models.Model):
--- user/mutation.py
+++ user/mutation.py
@@ -1,4 +1,5 @@
 import graphene
+import graphql_jwt
 
 from django.contrib.auth.hashers import make_password
 from graphql import GraphQLError
@@ -12,28 +13,23 @@ class CreateUser(graphene.Mutation):
         name = graphene.String(required=True)
         birth = graphene.Date(required=True)
         phone_number = graphene.String(required=True)
-        account = graphene.String(required=False)
+        account = graphene.String(required=True)
         password = graphene.String(required=True)
-        email = graphene.String(required=False)
-        is_user = graphene.Boolean(required=True)
+        email = graphene.String(required=True)
 
     user = graphene.Field(lambda: UserType)
 
     def mutate(self, info, **kwargs):
-        if kwargs["is_user"]:
-            if not kwargs.get("account", None) and not kwargs.get("email", None):
-                raise GraphQLError("Does Not Exist Key Account Or Email")
-            if not account_validation(kwargs["account"]):
-                raise GraphQLError("Incorrect Account Format")
-            if User.objects.filter(account=kwargs["account"]):
-                raise GraphQLError("Already Exist Account")
-            if User.objects.filter(email=kwargs["email"]):
-                raise GraphQLError("Already Exist Email")
-
+        if not account_validation(kwargs["account"]):
+            raise GraphQLError("Incorrect Account Format")
         if not password_validation(kwargs["password"]):
             raise GraphQLError("Incorrect Password Format")
         if not phone_number_validation(kwargs["phone_number"]):
             raise GraphQLError("Incorrect Phone Number Format")
+        if User.objects.filter(account=kwargs["account"]):
+            raise GraphQLError("Already Exist Account")
+        if User.objects.filter(email=kwargs["email"]):
+            raise GraphQLError("Already Exist Email")
         if User.objects.filter(phone_number=kwargs["phone_number"]):
             raise GraphQLError("Already Exist Phone_Number")
 
@@ -41,16 +37,16 @@ class CreateUser(graphene.Mutation):
             name=kwargs["name"],
             birth=kwargs["birth"],
             phone_number=kwargs["phone_number"],
-            account=kwargs.get("account", None),
+            account=kwargs["account"],
             password=make_password(kwargs["password"]),
-            email=kwargs.get("email", None),
-            is_user=kwargs["is_user"],
+            email=kwargs["email"],
         )
-
         user.save()
-
         return CreateUser(user=user)
 
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
