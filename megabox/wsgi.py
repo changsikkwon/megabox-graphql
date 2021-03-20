86a2026afd55018adb3ee753dece37f996e17f2f
chang sik kwon <kcs15987@gmail.com>
2021-03-21 00:04:08 +0900

    megabox

--- megabox/wsgi.py
+++ megabox/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'megabox.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "megabox.settings")
 
 application = get_wsgi_application()
