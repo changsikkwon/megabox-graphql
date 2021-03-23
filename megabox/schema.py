9a83a894df073f10ea9a4d61077160bc2c592bd9
chang sik kwon <kcs15987@gmail.com>
2021-03-24 06:42:52 +0900

    megabox

--- megabox/schema.py
+++ megabox/schema.py
@@ -3,12 +3,12 @@ import graphql_jwt
 
 from user.query import Query as user_query
 from user.mutation import Mutation as user_mutation
+from movie.query import Query as movie_query
 
-# from movie.query import Query as movie_query
 # from reservation.query import Query as reservation_query
 
 
-class Query(user_query):
+class Query(user_query, movie_query):
     pass
 
 
